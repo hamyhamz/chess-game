39c79b9e22d68e98c101ba903f2e5038a30272ad
HamyHamz <peter.hamran@gmail.com>
2020-01-28 20:39:07 +0100

    Additional files created

---
+++ Client/game.py
@@ -0,0 +1,11 @@
+"""Implementation of chess game object on client side of application
+Authors:
+   Peter Hamran        xhamra00@stud.fit.vutbr.cz
+Date:
+   20.01.2020
+"""
+
+
+class Game:
+    pass
+    #TODO
---
+++ Client/main.py
@@ -0,0 +1,9 @@
+"""Implementation of main to run a game as a player
+Authors:
+   Peter Hamran        xhamra00@stud.fit.vutbr.cz
+Date:
+   20.01.2020
+"""
+
+pass
+#TODO
