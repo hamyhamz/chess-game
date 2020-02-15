aa019aa21ab70632bfb8dece9852e1d526236f39
HamyHamz <peter.hamran@gmail.com>
2020-02-15 10:26:06 +0100

    Basic window created

--- Client/main.py
+++ Client/main.py
@@ -5,6 +5,37 @@ Date:
    20.01.2020
 """
 
+import pygame
+
 if __name__ == '__main__':
     print("Hello everyone!")
+
+    """
+    ********************************************************************
+        Code taken from https://pygame-gui.readthedocs.io/en/latest/quick_start.html
+        As a quick demonstration of initial window creation in pygame
+    """
+    pygame.init()
+
+    pygame.display.set_caption('Chess Game v.0')
+    window_surface = pygame.display.set_mode((800, 600))
+
+    backgound = pygame.Surface((800, 600))
+    backgound.fill(pygame.Color('#000000'))
+
+    is_running = True
+
+    while is_running:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                is_running = False
+
+        window_surface.blit(backgound, (0, 0))
+
+        pygame.display.update()
+
+    """
+    ********************************************************************
+    """
     #TODO
